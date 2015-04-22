fd9138975eb0b83d67e1e0261cb9153d42a0d807
javi1988 <jduran0831@yahoo.com>
2015-04-21 21:49:29 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,23 @@
+__author__ = 'Duran'
+
 import requests
 import urllib
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(urllib.urlopen('http://losangeles.craigslist.org/search/cta').read())
 
-for info in soup('p', {'class': 'row'}):
 
-    print info.getText()
+for row in soup('p', {'class': 'row'}):
+    price = row.getText().split()[0]
+    date = row.getText().split()[1] + " " + row.getText().split()[2]
+    car = " "
+    for i in range(3, 15):
+        try:
+            car += row.getText().split()[i] + " "
+        except IndexError:
+            print " "
+    print "price: " + price
+    print "date: " + date
+    print "car: " + car
+    print "-------------"
+
