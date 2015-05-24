92d4866ffae7e6b349721eb8f12ff53fbc1321c7
ktu5 <ktu5@users.noreply.github.com>
2015-05-23 19:43:30 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,23 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Scrapy settings for craigslist_sample project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
-
 BOT_NAME = 'craigslist_sample'
 
 SPIDER_MODULES = ['craigslist_sample.spiders']
 NEWSPIDER_MODULE = 'craigslist_sample.spiders'
 
-FEED_EXPORTERS = {
- 'jsonlines': 'scrapy.contrib.exporter.JsonLinesItemExporter',
-}
-FEED_FORMAT = 'jsonlines'
-FEED_URI = "items.json"
+ITEM_PIPELINES = ['craigslist_sample.pipelines.MongoDBPipeline', ]
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'craigslist_sample (+http://www.yourdomain.com)'
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "craigslist"
+MONGODB_COLLECTION = "cars"
