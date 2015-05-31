f2a7ce3990f90f80e53607d4b6bfa2b14b177f2b
javi1988 <jduran0831@yahoo.com>
2015-05-30 22:59:35 -0700

    Create items.py

---
+++ items.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.item import Item, Field
+
+class CraigslistSampleItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    title = Field()
+    date = Field()
+    detailedTime = Field()
+    model = Field()
+    price = Field()
+    odometer = Field()
+    status = Field()
+    area = Field()
+    link = Field()
+    location = Field()
+    pass
